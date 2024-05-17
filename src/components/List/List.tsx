618d7284605e635455735435af941b3c0eae7a36
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-05-17 22:57:25 +0200

    Update:changes List

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,48 +1,3 @@
-// import { onValue, query, ref } from "firebase/database";
-// import { useEffect, useState } from "react";
-// import { db } from "../../firebase/config";
-// import { Button } from "../Button/Button";
-// import { ItemList } from "./ItemList";
-// import { Psychology } from "../../type/Psycholog";
-
-// export const List = () => {
-//   const [data, setData] = useState<Psychology[]>([]);
-//   const [limit, setLimit] = useState<number>(3);
-
-//   const info = data[0]?.slice(0, limit);
-//   useEffect(() => {
-//     const get = query(ref(db));
-//     onValue(get, (snapshot) => {
-//       const res = snapshot.val();
-//       console.log(res);
-
-//       if (res) {
-//         setData(Object.values(res));
-//       } else {
-//         console.log("error");
-//       }
-//     });
-//   }, []);
-
-//   return (
-//     <>
-//       <ul className="mr-[128px] ml-[128px] ">
-//         {info?.map((item: Psychology, index: number) => (
-//           <ItemList key={index} item={item} />
-//         ))}
-//       </ul>
-//       <Button
-//         iconShow={false}
-//         prop="Load more"
-//         className="mx-[auto] mb-[100px]"
-//         onClick={() => {
-//           setLimit(limit + 3);
-//         }}
-//       />
-//     </>
-//   );
-// };
-
 import { onValue, query, ref } from "firebase/database";
 import { useEffect, useState } from "react";
 import { db } from "../../firebase/config";
@@ -54,7 +9,7 @@ export const List = () => {
   const [data, setData] = useState<Psychology[]>([]);
   const [limit, setLimit] = useState<number>(3);
 
-  const info = data.slice(0, limit); // Slice the data array directly
+  const info = data?.slice(0, limit);
   useEffect(() => {
     const get = query(ref(db));
     onValue(get, (snapshot) => {
@@ -62,7 +17,7 @@ export const List = () => {
       console.log(res);
 
       if (res) {
-        setData(Object.values(res));
+        setData(Object.values(res.data));
       } else {
         console.log("error");
       }
@@ -72,7 +27,7 @@ export const List = () => {
   return (
     <>
       <ul className="mr-[128px] ml-[128px] ">
-        {info.map((item: Psychology, index: number) => (
+        {info?.map((item: Psychology, index: number) => (
           <ItemList key={index} item={item} />
         ))}
       </ul>
